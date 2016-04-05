62ee8765e8df57c49fd290d6d84ea9182cf9d2c7
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2016-04-05 11:14:11 +0000

    Remove unnecessary packages

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 //var phantomjs = Npm.require('phantomjs');
 // var webshot = Npm.require('webshot');
 
-Meteor.methods({
-	'webshot': function(){
-			var phantomjs = Npm.require('phantomjs')
-			webshot('htpp://google.com', '/public/google.png',function(err){
-			if(err){
-				console.log(err);
-			}else{
-				console.log("done");
-			}
-		});
-	}
-});
+// Meteor.methods({
+// 	'webshot': function(){
+// 			var phantomjs = Npm.require('phantomjs')
+// 			webshot('htpp://google.com', '/public/google.png',function(err){
+// 			if(err){
+// 				console.log(err);
+// 			}else{
+// 				console.log("done");
+// 			}
+// 		});
+// 	}
+// });
